be8b406b96ded7bcc61c8041eb28fb6beca52c8f
Ingrid Bini <binifidelis@gmail.com>
2025-08-06 11:16:48 -0300

    Delete Java-Alura/java-modulos directory

--- Java-Alura/java-modulos/src/DesafioPoo/CalculadoraPoo.java
+++
@@ -1,8 +0,0 @@
-package MinhaPrimeiraAplicacao.DesafioPoo;
-
-public class CalculadoraPoo {
-    //método
-    int numeroDobrado(int numero){
-        return numero * 2;
-    }
-}
--- Java-Alura/java-modulos/src/MinhaPrimeiraAplicacao/DesafioClasses/Estudos.java
+++
@@ -1,7 +0,0 @@
-package DesafioClasses;
-
-public class Estudos {
-    public static void main(String[] args) {
-        System.out.println("Anotações");
-    }
-}
